f52f928566b0e8ea1f6b05b348109eecce50af41
Sam Bowen <samhbowen@gmail.com>
2025-02-28 14:54:04 +0000

    import NavBr, PokemonList,

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+// src/components/NavBar/NavBar.jsx
+
+// src/components/NavBar/NavBar.jsx
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Pokemon</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
